6e984ffe4984dbd6c65480e820fe982f367c69f1
Shrimaj <shrimaj.mehta60@gmail.com>
2021-12-02 11:17:23 +0530

    Add project files.

---
+++ EmpWageProb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageProb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Emp wage Problem");
+        }
+    }
+}
